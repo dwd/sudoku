48435bf4b4b1c7280a9d32825bdcbe844c225ec2
Dave Cridland <dave@cridland.net>
2023-12-30 16:54:07 +0000

    Terraform beginnings

---
+++ terraform/providers.tf
@@ -0,0 +1,21 @@
+terraform{
+  required_providers {
+    gandi = {
+      version = "~> 2.0.0"
+      source = "go-gandi/gandi"
+    }
+    acme = {
+      version = "~> 2.0"
+      source = "vancluever/acme"
+    }
+    aws = {
+      version = "~> 5.0"
+      source = "hashicorp/aws"
+    }
+  }
+}
+
+provider "aws" {
+  region = "eu-west-1"
+}
+
